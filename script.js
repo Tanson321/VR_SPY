e3ec1fc44ebbc6bdc19f338599a1cace6647f170
Tanson321 <astronaut.tanson@gmail.com>
2019-07-02 02:32:39 +0900

    added an image on github

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
 
   var vrView = new VRView.Player('#vrview', {
-    image: 'image.jpg',
+    image: 'https://raw.githubusercontent.com/Tanson321/VR_SPY/master/image.jpg',
     is_stereo: true
   });
 }
