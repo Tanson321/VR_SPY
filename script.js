fc59c0a7db5754bf55a7a5fb602737c854c0f9e3
Tanson321 <astronaut.tanson@gmail.com>
2019-07-02 02:54:37 +0900

    images size fixed

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function onVrViewLoad() {
 
   var vrView = new VRView.Player('#vrview', {
     image: 'https://raw.githubusercontent.com/Tanson321/image_stock/master/worldmap.jpg',
-    is_stereo: false
+    width:  '100%',
+    height: '100%',
+    is_autopan_off: true
+
   });
 }
 
