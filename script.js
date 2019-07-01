46e1039294c4e03f50887d2b945f6f0ccb309037
Tanson321 <astronaut.tanson@gmail.com>
2019-07-02 02:50:23 +0900

    changed the image

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
 
   var vrView = new VRView.Player('#vrview', {
-    image: 'https://raw.githubusercontent.com/Tanson321/VR_SPY/master/image.jpg',
-    is_stereo: true
+    image: 'https://raw.githubusercontent.com/Tanson321/image_stock/master/worldmap.jpg',
+    is_stereo: false
   });
 }
 
