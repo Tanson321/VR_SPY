839c70576fc7002ed1bc72c4905554cf7f22fcb6
Tanson321 <astronaut.tanson@gmail.com>
2019-07-03 20:30:36 +0900

    added a hotspot

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 window.addEventListener('load', onVrViewLoad);
 
 function onVrViewLoad() {
-
-  var vrView = new VRView.Player('#vrview', {
-    image: 'https://raw.githubusercontent.com/Tanson321/image_stock/master/001.jpg',
+  vrView = new VRView.Player('#vrview', {
+    image: 'https://raw.githubusercontent.com/Tanson321/image_stock/master/map_tomon.jpg',
     width:  '100%',
     height: '100%',
-    is_autopan_off: true
-
+    is_autopan_off: true,
+    is_vr_off: true,
   });
-}
+  vrView.on('ready', onVRViewReady);
+  vrView.on('click', onVRViewClick);
+};
 
+function onVRViewReady(e){
+  vrView.addHotspot('spot1', {
+    pitch: 0,
+    yaw: 0,
+    radius: 0.05,
+    distance: 1
+});
+};
+
+function onVRViewClick(e){
+  if(e.id == 'spot1'){
+      alert("ホットスポット1がクリックされました！");
+  }
+};
